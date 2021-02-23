9940ba14057a4806f8981fda196eba1ac8c278cf
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-02-24 01:56:23 +0530

    Added Header component

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCaU7fsiRYUfPxtyoAyoKkvpidkd-LWMj8",
+    authDomain: "facebook-clone-5c32f.firebaseapp.com",
+    projectId: "facebook-clone-5c32f",
+    storageBucket: "facebook-clone-5c32f.appspot.com",
+    messagingSenderId: "577471623893",
+    appId: "1:577471623893:web:fba05f5bbaf12dfb1a285c",
+    measurementId: "G-BBEH4E4VN1"
+  };
