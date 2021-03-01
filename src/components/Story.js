bc9e405f7904fb4e0084d3e18f3d680ece1960e4
rajnish1999 <rajnishtiwarihrt@gmail.com>
2021-03-02 00:26:22 +0530

    Styled Story component

--- src/components/Story.js
+++ src/components/Story.js
@@ -4,8 +4,8 @@ import '../componentsStyle/Story.css';
 
 function Story({ image, prfileSrc, title }) {
     return (
-        <div style={{ backgroundImage: `url(${image})`}} className="Story">
-            <Avatar>RT</Avatar>
+        <div style={{ backgroundImage: `url(${image})`}} className="story">
+            <Avatar className="story__avatar">RT</Avatar>
             <h4>{title}</h4>
         </div>
     )
--- src/components/StoryReel.js
+++ src/components/StoryReel.js
@@ -7,22 +7,22 @@ function StoryReel() {
         <div className="storyReel">
             <Story 
                 image="https://picsum.photos/200"
-                profileSrc="https://picsum.photos/200"
+                profileSrc="https://picsum.photos/201"
                 title="Rajnish Tiwari"
             />
             <Story 
                 image="https://picsum.photos/200"
-                profileSrc="https://picsum.photos/200"
+                profileSrc="https://picsum.photos/202"
                 title="Rajnish Tiwari"
             />
             <Story 
                 image="https://picsum.photos/200"
-                profileSrc="https://picsum.photos/200"
+                profileSrc="https://picsum.photos/203"
                 title="Rajnish Tiwari"
             />
             <Story 
                 image="https://picsum.photos/200"
-                profileSrc="https://picsum.photos/200"
+                profileSrc="https://picsum.photos/204"
                 title="Rajnish Tiwari"
             />
             <Story 
